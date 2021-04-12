be6eb896a7ca2b766286a29005e9ee08dfbd13a2
ol222hf <ol222hf@student.lnu.se>
2021-04-12 11:58:53 +0200

    adding /users route

--- api/src/routes/indexRouter.ts
+++ api/src/routes/indexRouter.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import ExampleRouter from './exampleRouter';
+import UserRouter from './userRouter';
 
 export default class IndexRouter {
     public router: express.Router = express.Router()
     private exampleRouter: ExampleRouter = new ExampleRouter()
+    private userRouter: UserRouter = new UserRouter()
+
     constructor() {
         this.initializeRoutes();
     }
 
-    public initializeRoutes() {
+    public initializeRoutes(): void {
+        this.router.use('/users', this.userRouter.expressRouter);
         this.router.use('/example', this.exampleRouter.expressRouter);
     }
 }
-
-
