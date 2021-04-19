8aecbeb340db87e4920e90c972e4949f65e713e3
ol222hf <ol222hf@student.lnu.se>
2021-04-19 22:27:48 +0200

    find user, save("update") new page

--- api/src/services/pageService.ts
+++ api/src/services/pageService.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import createHttpError from 'http-errors';
 import Page, { IPage } from '../models/page';
 import { URL } from 'url';
+import User from 'models/user';
 
 export interface pageData {
     href: string
@@ -22,10 +23,29 @@ export default class PageService {
         }
     }
 
-    public async updatePage(req: Request): Promise<IPage> {
-        try {          
-            return await Page.findAndUpdate(new URL(req.body.address), req.params.id);
+    public async updatePage(req: any): Promise<void> {
+        try {
+            // Find user
+            const user = await User.findOne({email: req.user.email});
+        
+            // User not found...
+            if (!user) {
+                // ... 404
+                throw createHttpError(404, 'User not found');
+            }
+
+            // Add "new" address if not exists
+            const page = await Page.findOrCreate(new URL(req.body.address), req.params.id);
+            
+            // find old address in array using :id 
+            // replace with "new" address id
+            const index = user.pageIds.findIndex(req.params.id);
+            pageIds[index] = page.id;
+            user.save();
+            
         } catch (error) {
+            console.log('error in service: ', error);
+
             if(error.code === 'ERR_INVALID_URL') {
                 throw createHttpError(400, `${error.input} is not a valid address.`);
             }
