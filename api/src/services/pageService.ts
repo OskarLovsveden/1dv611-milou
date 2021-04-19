81278b7ec8cd863b751cd4cb7b95c79ef921be62
MattRul <mattias.ruljeff@gmail.com>
2021-04-19 16:26:32 +0200

    Final fix, await

--- api/src/services/pageService.ts
+++ api/src/services/pageService.ts
@@ -1,4 +1,3 @@
-import { Request } from 'express';
 import createHttpError from 'http-errors';
 import Page, { IPage } from '../models/page';
 import User from '../models/user';
@@ -13,19 +12,30 @@ export interface pageData {
 export default class PageService {
 
     // Change from any to request. -------------------------------------------
-    public async createPage(req: any): Promise<void> {
+    public async createPage(req: any): Promise<IPage> {
         try {
-            const foundPage = Page.findOne({domain: req.body.address});
+            const foundPage = await Page.findOne({address: req.body.address});
             if(!foundPage){
                 const newPage = await Page.insert(new URL(req.body.address));
-
-                // Change to req.user.email -------------------------------------------------------
-                const user = await User.findOne({email: 'ravenraskar3@hotmail.com'});
+                
+                // Change to req.user.email or use jwt token -------------------------------------------------------
+                const user = await User.findOne({email: req.user.email});
                 if (user){
                     user.domainIds.push(newPage.id);
                     await user.save();
                 }
+                return newPage;
+            } else {
+                const user = await User.findOne({email: req.user.email});
+                if (user){
+                    if(!user.domainIds.includes(foundPage.id)){
+                        user.domainIds.push(foundPage.id);
+                        await user.save();
+                    }
+                }
+                return foundPage;
             }
+            
         } catch (error) {
             if(error.code === 'ERR_INVALID_URL') {
                 throw createHttpError(400, `${error.input} is not a valid address.`);
