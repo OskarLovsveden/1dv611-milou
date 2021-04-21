f612d38695367b78846c29ef51c9a3cf48d23e14
MattRul <mattias.ruljeff@gmail.com>
2021-04-21 09:22:23 +0200

    Fixing presave user bug in pages

--- api/src/services/pageService.ts
+++ api/src/services/pageService.ts
@@ -14,30 +14,26 @@ export default class PageService {
     // Change from any to request. -------------------------------------------
     public async createPage(req: any): Promise<IPage> {
         try {
-            console.log(req.body.address);
             const foundPage = await Page.findOne({address: req.body.address});
 
-            console.log(foundPage);
             if(foundPage){
-                console.log('skapa inte ny');
                 const user = await User.findOne({email: req.user.email});
                 if (user){
                     if(!user.pageIds.includes(foundPage.id)){
                         user.pageIds.push(foundPage.id);
-                        await user.save();
+                        await User.updateOne({_id: user.id}, {pageIds: user.pageIds});
                     }
                 }
                 return foundPage;
-            } 
-                
-            console.log('SKapa ny');
+            }
+
             const newPage = await Page.insert(new URL(req.body.address));
             
             // Change to req.user.email or use jwt token -------------------------------------------------------
             const user = await User.findOne({email: req.user.email});
             if (user){
                 user.pageIds.push(newPage.id);
-                await user.save();
+                await User.updateOne({_id: user.id}, {pageIds: user.pageIds});
             }
             return newPage;
             
