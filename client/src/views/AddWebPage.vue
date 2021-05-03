b2826b1d61ab769497e3ea0d057b29c7606bcdbe
MattRul <mattias.ruljeff@gmail.com>
2021-05-03 12:54:39 +0200

    Adding client

---
+++ client/src/views/AddWebPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="addwebpage">
+    <h1>Add web page</h1>
+    <Hej title="iohasdjh"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Hej from "../components/Hej.vue"
+
+@Options({
+  components: {
+    Hej
+  },
+})
+export default class AddWebPage extends Vue {}
+</script>
