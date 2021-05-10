5790679b1dbb0440f7b3fd8cc9d6b96f6c22c823
MattRul <mattias.ruljeff@gmail.com>
2021-05-10 10:37:15 +0200

    Adding webpage and axios helper

---
+++ client/src/helpers/AxiosHelper.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export default class AxiosHelper {
+    private cookieValue: string | undefined
+
+    constructor () {
+        // Gets the cookie containing the JWT
+        this.cookieValue = document?.cookie?.split('; ')?.find(row => row?.startsWith('token='))?.split('=')[1]
+    }
+
+    // Post request using the current JWT
+    async post(url: string, data: any) {
+        await axios({
+            method: 'POST',
+            url: url,
+            headers: {
+                authorization: 'Bearer ' + this.cookieValue
+            },
+            data: data
+        })
+    }
+
+    // Get request using the current JWT
+    async get(url: string) {
+        const response = await axios({
+            method: 'GET',
+            url: url,
+            headers: {
+                authorization: 'Bearer ' + this.cookieValue
+            }
+        })
+
+        console.log(response, "data")
+        console.log(response.data)
+    }
+}
