0249e4348ee24968d30ee5106ad44d344569e226
ol222hf <ol222hf@student.lnu.se>
2021-05-17 15:20:45 +0200

    implemented vuex user state

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,33 +1,24 @@
 <template>
   <div class="home">
-    <div v-if="loading">
-      <h1>Loading...</h1>
-    </div>
-    <div v-else>
       <div v-if="isAuthenticated">
         <button @click="logout">Logout</button>
-        <h1>{{this.user}}</h1>
+        <h1>{{getUserEmail}}</h1>
         <Profile />
       </div>
       <div v-else>
-
         <div v-if="!registerUser">
-          <h1>Milou Project</h1>
           <h3>Login existing user</h3>
-          <LoginForm @logged-in="checkUser" />
+          <LoginForm />
           <span>New user? Register </span>
           <a @submit.prevent @click="flipRegisterUser" href="#">here</a>
         </div>
-        
         <div v-else>
-          <h1>Milou Project</h1>
           <h3>Register new user</h3>
-          <RegisterForm @logged-in="checkUser" />
+          <RegisterForm />
           <span>Already registered? Login </span>
           <a @submit.prevent @click="flipRegisterUser" href="#">here</a>
         </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -37,9 +28,6 @@ import { Options, Vue } from 'vue-class-component';
 import LoginForm from '../components/LoginForm.vue';
 import RegisterForm from '../components/RegisterForm.vue';
 import Profile from '../components/Profile.vue';
-
-import AxiosHelper from '../helpers/AxiosHelper';
-const axios = new AxiosHelper();
 
 import Cookie from '../helpers/Cookie';
 const cookie = new Cookie('token');
@@ -52,36 +40,27 @@ const cookie = new Cookie('token');
     },
     data() {
         return {
-            user: '',
-            loading: true,
-            isAuthenticated: false,
             registerUser: false
         };
     },
     methods: {
-        async checkUser() {
-            const response = await axios.post('/auth/authenticate');
-            this.user = response?.data?.authenticatedUser;
-            this.isAuthenticated = response.status === 200;
-        },
-        flipIsAuthenticated() {
-            this.isAuthenticated = !this.isAuthenticated;
-        },
         flipRegisterUser() {
-            this.registerUser = !this.registerUser;
+          this.registerUser = !this.registerUser;
         },
         logout() {
-            cookie.delete();
-            this.flipIsAuthenticated();
+          cookie.delete();
+          this.$store.commit('FLIP_IS_AUTHENTICATED');
         }
     },
-    async mounted() {
-        if (cookie.get()) {
-            await this.checkUser();
-        }
-    
-        this.loading = false;
-    }
+    computed: {
+      isAuthenticated() {
+        return this.$store.state.user.isAuthenticated
+      },
+      getUserEmail() {
+        return this.$store.state.user.email
+      },
+    },
+
 })
 export default class Home extends Vue {}
 </script>
