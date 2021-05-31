57ac59b96c9528992f7db917b6123a52ef788d67
MattRul <mr223mi@student.lnu.se>
2021-05-31 09:10:55 +0200

    Added spinner as component

--- client/src/components/ModalGPSI.vue
+++ client/src/components/ModalGPSI.vue
@@ -9,10 +9,8 @@
 
           <div class="modal-body">
             <slot name="body">
-              <div v-if="loader" id="loader-container">
-                <h3>Loading...</h3>
-                <p>This may take up to one minute</p>
-                <div id="loader"></div>
+              <div v-if="loader">
+                <LoadingSpinner />
               </div>
               <div v-else>
                 <Chart :data="measureResult" />
@@ -40,10 +38,12 @@ import AxiosHelper from "../helpers/AxiosHelper";
 const axios = new AxiosHelper();
 
 import Chart from "./DataChart.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 
 @Options({
   components: {
     Chart,
+    LoadingSpinner,
   },
   props: {
     address: {
@@ -151,29 +151,5 @@ export default class ModalGPSI extends Vue {}
 #result {
   display: flex;
   flex-direction: column;
-}
-
-#loader-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-#loader {
-  border: 6px solid #f3f3f3; /* Light grey */
-  border-top: 6px solid #3498db; /* Blue */
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  animation: spin 2s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
 }
 </style>
--- client/src/components/WebpageForm.vue
+++ client/src/components/WebpageForm.vue
@@ -1,80 +1,112 @@
 <template>
-    <form v-on:submit.prevent autocomplete="on">
-        <input :placeholder="placeholder" type="url" name="url" autocomplete="on" v-model="url" required>
-        <button v-if="address" @click="updateWebPage" type="submit">Update</button>
-        <button v-else @click="addWebPage" type="submit">Add</button>
-        <div id="radio-buttons">
-          <span>Test interval: </span>
-          <input type="radio" name="interval" value="Daily" v-model="interval" required>
-          <label for="daily">Daily</label>
-          <input type="radio" name="interval" value="Weekly" v-model="interval">
-          <label for="weekly">Weekly</label>
-          <input type="radio" name="interval" value="Monthly" v-model="interval">
-          <label for="monthly">Monthly</label>
-        </div>
-    </form>
+  <div v-if="loading">
+    <LoadingSpinner />
+  </div>
+  <form v-else v-on:submit.prevent autocomplete="on">
+    <input
+      :placeholder="placeholder"
+      type="url"
+      name="url"
+      autocomplete="on"
+      v-model="url"
+      required
+    />
+    <button v-if="address" @click="updateWebPage" type="submit">Update</button>
+    <button v-else @click="addWebPage" type="submit">Add</button>
+    <div id="radio-buttons">
+      <span>Test interval: </span>
+      <input
+        type="radio"
+        name="interval"
+        value="Daily"
+        v-model="interval"
+        required
+      />
+      <label for="daily">Daily</label>
+      <input type="radio" name="interval" value="Weekly" v-model="interval" />
+      <label for="weekly">Weekly</label>
+      <input type="radio" name="interval" value="Monthly" v-model="interval" />
+      <label for="monthly">Monthly</label>
+    </div>
+  </form>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 
-import AxiosHelper from '../helpers/AxiosHelper';
+import AxiosHelper from "../helpers/AxiosHelper";
 const axios = new AxiosHelper();
 
-@Options({
-    props: {
-        address: {
-            type: String
-        },
-        testInterval: {
-            type: String
-        },
-        pageID: {
-            type: String
-        }
-    },
-    data() {
-        return {
-            url: '',
-            interval: ''
-        };
-    },
-    computed: {
-        placeholder() { 
-            return this.address ? this.address : 'http://example.com';
-        },
-    },
-    methods: {
-        async addWebPage(){
-            if(this.interval){
-                try {
-                    await axios.post('/pages', {address: this.url, testInterval: this.interval});
-                    this.$toast.success("Page added")
-                    this.url = '';
-                    await this.$store.dispatch('loadPages');
-                } catch (error) {
-                    this.$toast.error("Error when adding page")
-                }
-            }
-        },
-        async updateWebPage() {
-            try {
-                await axios.update('/pages/' + this.pageID, {address: this.url, testInterval: this.interval});
-                this.$toast.success("Page updated")
-                await this.$store.dispatch('loadPages');
-            } catch (error) {
-                this.$toast.error("Error when updating page")
-                
-            }
-        }
-    }
-})
+import LoadingSpinner from "./LoadingSpinner.vue";
 
+@Options({
+  components: {
+    LoadingSpinner,
+  },
+  props: {
+    address: {
+      type: String,
+    },
+    testInterval: {
+      type: String,
+    },
+    pageID: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      url: "",
+      interval: "",
+      loading: false,
+    };
+  },
+  computed: {
+    placeholder() {
+      return this.address ? this.address : "http://example.com";
+    },
+  },
+  methods: {
+    async addWebPage() {
+      if (this.interval) {
+        try {
+          this.loading = true;
+          await axios.post("/pages", {
+            address: this.url,
+            testInterval: this.interval,
+          });
+          this.$toast.success("Page added");
+          this.loading = false;
+          this.url = "";
+          await this.$store.dispatch("loadPages");
+        } catch (error) {
+          this.loading = false;
+          this.$toast.error("Error when adding page");
+        }
+      }
+    },
+    async updateWebPage() {
+      try {
+        this.loading = true;
+        await axios.update("/pages/" + this.pageID, {
+          address: this.url,
+          testInterval: this.interval,
+        });
+        this.loading = false;
+        this.$toast.success("Page updated");
+        await this.$store.dispatch("loadPages");
+      } catch (error) {
+        this.loading = false;
+        this.$toast.error("Error when updating page");
+      }
+    },
+  },
+})
 export default class WebpageForm extends Vue {}
 </script>
 
 <style scoped>
-  #radio-buttons {
-    margin-top: 10px;
-  }
+#radio-buttons {
+  margin-top: 10px;
+}
 </style>
