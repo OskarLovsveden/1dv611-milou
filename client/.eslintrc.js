70fbcb0810529f03164905ea9706c92f48cd1c9d
MattRul <mr223mi@student.lnu.se>
2021-05-31 08:35:46 +0200

    Changing logo to image, adding graph on direct search

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  parserOptions: {
-      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-      sourceType: 'module', // Allows for the use of imports
-      ecmaFeatures: { }
-  },
-  settings: { },
-  extends: [
-  ],
-  rules: {
-      // we only want single quotes
-      'quotes': ['error', 'single'],
-      // we want to force semicolons
-      'semi': ['error', 'always'],
-      // we use 4 spaces to indent our code
-      'indent': ['error', 4],
-      // we want to avoid useless spaces
-      'no-multi-spaces': ['error']
-  }
-};
+  env: {
+  node: true
+},
+extends: [
+  'plugin:vue/vue3-essential',
+  'eslint:recommended',
+  '@vue/typescript/recommended'
+],
+parserOptions: {
+  ecmaVersion: 2020,
+  sourceType: 'module'
+},
+rules: {
+  'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+},
+}
--- client/src/components/ModalGPSI.vue
+++ client/src/components/ModalGPSI.vue
@@ -3,7 +3,6 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-
           <div class="modal-header">
             <h3 name="header">Result from GPSI measure</h3>
           </div>
@@ -12,17 +11,12 @@
             <slot name="body">
               <div v-if="loader" id="loader-container">
                 <h3>Loading...</h3>
+                <p>This may take up to one minute</p>
                 <div id="loader"></div>
               </div>
-              
-            <div v-if="measureResult" id="result">
-              <div v-for="(item, index) in measureResult" :key="index">
-                <p>Total score: {{item.totalScore}}</p>
-                <div v-for="(category, index) in item.categories" :key="index">
-                  <p>{{category.title}}: {{category.score}}</p>
-                </div>
+              <div v-else>
+                <Chart :data="measureResult" />
               </div>
-            </div>
             </slot>
           </div>
 
@@ -40,45 +34,52 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 
-import AxiosHelper from '../helpers/AxiosHelper';
-const axios = new AxiosHelper()
+import AxiosHelper from "../helpers/AxiosHelper";
+const axios = new AxiosHelper();
+
+import Chart from "./DataChart.vue";
 
 @Options({
-    components: {
+  components: {
+    Chart,
+  },
+  props: {
+    address: {
+      type: String,
     },
-    props: {
-        address: {
-            type: String
-        }
+  },
+  data() {
+    return {
+      loader: true,
+      measureResult: null,
+    };
+  },
+  methods: {
+    emitCloseEvent() {
+      this.$emit("close");
     },
-    data() {
-      return {
-        loader: false,
-        measureResult: null,
-      }
-    },
-    methods: {
-        emitCloseEvent() {
-            this.$emit('close');
-        }
-    },
-    async mounted() {
-      this.loader = true;
-      const result = await axios.post("/gpsi/measure", { addresses: [this.address] })
-      if (result) {
-        this.loader = false
-        this.measureResult = await result.data
-      }
+  },
+  async mounted() {
+    this.loader = true;
+    const result = await axios.post("/gpsi/measure", {
+      addresses: [this.address],
+    });
+    if (result) {
+      this.measureResult = await result.data;
+      this.measureResult.forEach((element: any) => {
+        console.log(Object.keys(element), "keys");
+        console.log(Object.values(element), "values");
+      });
+      this.loader = false;
     }
+  },
 })
-
 export default class ModalGPSI extends Vue {}
 </script>
 
 <style scoped>
-
 .modal-mask {
   position: fixed;
   z-index: 9998;
@@ -147,10 +148,10 @@ export default class ModalGPSI extends Vue {}
   justify-content: flex-end;
 }
 
-  #result {
-    display: flex;
-    flex-direction: column;
-  } 
+#result {
+  display: flex;
+  flex-direction: column;
+}
 
 #loader-container {
   display: flex;
@@ -159,17 +160,20 @@ export default class ModalGPSI extends Vue {}
 }
 
 #loader {
-border: 6px solid #f3f3f3; /* Light grey */
-border-top: 6px solid #3498db; /* Blue */
-border-radius: 50%;
-width: 40px;
-height: 40px;
-animation: spin 2s linear infinite;
+  border: 6px solid #f3f3f3; /* Light grey */
+  border-top: 6px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 2s linear infinite;
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
-
 </style>
