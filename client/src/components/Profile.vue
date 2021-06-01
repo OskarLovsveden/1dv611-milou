5465639a76e10b5a14e686f638181ffe46001794
MattRul <mr223mi@student.lnu.se>
2021-06-01 15:07:58 +0200

    Small CSS change on profile

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -5,67 +5,65 @@
     </div>
     <div class="addwebpage">
       <div class="domain-menu">
-        <DomainSelector/>
+        <DomainSelector />
         <button id="show-modal" @click="toggleModal">Add new webpage</button>
       </div>
-      <Modal v-if="showModal" @close="toggleModal"/>
+      <Modal v-if="showModal" @close="toggleModal" />
       <PageList />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 import DirectSearch from "../components/DirectSearch.vue";
 import DomainSelector from "../components/DomainSelector.vue";
 import PageList from "../components/PageList.vue";
 import Modal from "../components/Modal.vue";
 
 @Options({
-    components: {
-        DomainSelector,
-        DirectSearch,
-        PageList,
-        Modal
+  components: {
+    DomainSelector,
+    DirectSearch,
+    PageList,
+    Modal,
+  },
+  async mounted() {
+    await this.getPages();
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    async getPages() {
+      await this.$store.dispatch("loadPages");
     },
-    async mounted() {
-        await this.getPages();
+    toggleModal() {
+      this.showModal = !this.showModal;
     },
-    data() {
-        return {
-            showModal: false
-        };
-    },
-    methods: {
-        async getPages() {
-            await this.$store.dispatch('loadPages');
-        },
-        toggleModal() {
-          this.showModal = !this.showModal;
-        }
-    }
+  },
 })
-
 export default class Profile extends Vue {}
 </script>
 
 <style scoped>
-  .addwebpage {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+.addwebpage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
-  .domain-menu {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    width: 100%;
-  }
+.domain-menu {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+}
 
-  .domain-menu form {
-    margin-right: 10px;
-  }
-
+.domain-menu form {
+  margin-right: 10px;
+}
 </style>
